e4cdfa3077f9a39979fe47bcb276206a6da89d8c
Kelsey Steeves <kelseysteeves@gmail.com>
2018-05-05 21:34:36 -0400

    next part of tutorial, colored square

--- PixelPoint/PixelPointRenderer.mm
+++ PixelPoint/PixelPointRenderer.mm
@@ -16,17 +16,24 @@
 const GLchar* vertexSource = R"glsl(
 #version 150 core
 in vec2 position;
+in vec3 color;
+
+out vec3 Color;
+
 void main()
 {
+    Color = color;
     gl_Position = vec4(position, 0.0, 1.0);
 }
 )glsl";
+
 const GLchar* fragmentSource = R"glsl(
 #version 150 core
+in vec3 Color;
 out vec4 outColor;
 void main()
 {
-    outColor = vec4(1.0, 1.0, 1.0, 1.0);
+    outColor = vec4(Color, 1.0);
 }
 )glsl";
 
@@ -45,14 +52,27 @@ void main()
         GLuint vbo;
         glGenBuffers(1, &vbo);
         
-        GLfloat vertices[] = {
-            0.0f,  0.5f,
-            0.5f, -0.5f,
-            -0.5f, -0.5f
+        float vertices[] = {
+            -0.5f,  0.5f, 1.0f, 0.0f, 0.0f, // Top-left
+            0.5f,  0.5f, 0.0f, 1.0f, 0.0f, // Top-right
+            0.5f, -0.5f, 0.0f, 0.0f, 1.0f, // Bottom-right
+            -0.5f, -0.5f, 1.0f, 1.0f, 1.0f  // Bottom-left
         };
         
         glBindBuffer(GL_ARRAY_BUFFER, vbo);
         glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+        
+        // Create an element array
+        GLuint ebo;
+        glGenBuffers(1, &ebo);
+        
+        GLuint elements[] = {
+            0, 1, 2,
+            2, 3, 0
+        };
+        
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
+        glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(elements), elements, GL_STATIC_DRAW);
         
         // Create and compile the vertex shader
         GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
@@ -75,14 +95,23 @@ void main()
         // Specify the layout of the vertex data
         GLint posAttrib = glGetAttribLocation(shaderProgram, "position");
         glEnableVertexAttribArray(posAttrib);
-        glVertexAttribPointer(posAttrib, 2, GL_FLOAT, GL_FALSE, 0, 0);
+        glVertexAttribPointer(posAttrib, 2, GL_FLOAT, GL_FALSE, 5*sizeof(float), 0);
+        
+        GLint colAttrib = glGetAttribLocation(shaderProgram, "color");
+        glEnableVertexAttribArray(colAttrib);
+        glVertexAttribPointer(colAttrib, 3, GL_FLOAT, GL_FALSE, 5*sizeof(float), (void*)(2*sizeof(float)));
     }
     
     return self;
 }
 
 - (void) render {
-    glDrawArrays(GL_TRIANGLES, 0, 3);
+    // Clear the screen to black
+    glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
+    
+    // Draw a rectangle from the 2 triangles using 6 indices
+    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 }
 
 - (void) dealloc {
